68b155e78701d42abfd428be43677dbb00c85439
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2023-03-20 13:09:39 -0300

    Não aguento mais olhar para isso

--- projeto/src/main/java/br/edu/criandoapi/projeto/controler/UsuarioController.java
+++ projeto/src/main/java/br/edu/criandoapi/projeto/controler/UsuarioController.java
@@ -1,19 +1,32 @@
 package br.edu.criandoapi.projeto.controler;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.criandoapi.projeto.DAO.IUsuario;
+import br.edu.criandoapi.projeto.model.Usuario;
 
 @RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
 
     @Autowired
     private IUsuario dao;
 
     @GetMapping("/usuarios")
-    public String texto() {
-        return dao.toString();
+    public List<Usuario> texto() {
+        return (List<Usuario>) dao.findAll();
+    }
+
+    @PostMapping
+    public Usuario criarUsuario(@RequestBody Usuario usuario) {
+        Usuario usuarioNovo = dao.save(usuario);
+        return usuarioNovo;
     }
 }
--- projeto/src/main/java/br/edu/criandoapi/projeto/model/Usuario.java
+++ projeto/src/main/java/br/edu/criandoapi/projeto/model/Usuario.java
@@ -18,7 +18,7 @@ public class Usuario {
     private int id;
 
     @Column(name = "nome", length = 50, nullable = false)
-    private String nome_completo;
+    private String nome;
 
     @Column(name = "email", length = 50, nullable = false)
     private String email;
@@ -33,9 +33,6 @@ public class Usuario {
         this.id = id;
     }
 
-    public void setNome_completo(String nome_completo) {
-        this.nome_completo = nome_completo;
-    }
 
     public void setEmail(String email) {
         this.email = email;
@@ -53,9 +50,6 @@ public class Usuario {
         return id;
     }
 
-    public String getNome_completo() {
-        return nome_completo;
-    }
 
     public String getEmail() {
         return email;
@@ -67,6 +61,16 @@ public class Usuario {
 
     public String getTel() {
         return tel;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
 }
