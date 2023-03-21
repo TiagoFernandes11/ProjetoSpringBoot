015da0d26fb373ff24e1f22f92755515710d128b
Tiago Fernandes Ribeiro <tiagofernandesribeiro@yahoo.com.br>
2023-03-21 12:07:09 -0300

    Update UsuarioController.java

--- projeto/src/main/java/br/edu/criandoapi/projeto/controler/UsuarioController.java
+++ projeto/src/main/java/br/edu/criandoapi/projeto/controler/UsuarioController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,4 +30,11 @@ public class UsuarioController {
         Usuario usuarioNovo = dao.save(usuario);
         return usuarioNovo;
     }
+
+    @PutMapping
+    public Usuario editUsuario(@RequestBody Usuario usuario) {
+        Usuario usuarioNovo = dao.save(usuario);
+        return usuarioNovo;
+    }
+    
 }
