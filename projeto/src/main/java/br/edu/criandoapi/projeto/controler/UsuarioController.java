41d190416f0f5d57f7c922412be88f2d772cbf78
Tiago Fernandes Ribeiro <tiagofernandesribeiro@yahoo.com.br>
2023-03-23 12:39:57 -0300

    @DeleteMapping by id

--- projeto/src/main/java/br/edu/criandoapi/projeto/controler/UsuarioController.java
+++ projeto/src/main/java/br/edu/criandoapi/projeto/controler/UsuarioController.java
@@ -1,9 +1,12 @@
 package br.edu.criandoapi.projeto.controler;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -35,6 +38,13 @@ public class UsuarioController {
     public Usuario editUsuario(@RequestBody Usuario usuario) {
         Usuario usuarioNovo = dao.save(usuario);
         return usuarioNovo;
+    }
+
+    @DeleteMapping("/{id}")
+    public Optional<Usuario> deleteUsuario(@PathVariable Integer id){
+        Optional<Usuario> usuario = dao.findById(id);
+        dao.deleteById(id);
+        return usuario;
     }
     
 }
