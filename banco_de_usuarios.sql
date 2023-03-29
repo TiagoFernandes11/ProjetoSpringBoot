387a464ecaf2d7c5d3813b3f2f62c158ed3073c2
Tiago Fernandes Ribeiro <tiagofernandesribeiro@yahoo.com.br>
2023-03-29 15:13:20 -0300

    Create banco_de_usuarios.sql

---
+++ banco_de_usuarios.sql
@@ -0,0 +1,17 @@
+drop database if exists banco_de_usuarios;
+
+create database if not exists banco_de_usuarios;
+
+use banco_de_usuarios;
+
+create table usuario(
+id int(3) primary key auto_increment,
+nome varchar(50) not null,
+email varchar(30) not null unique,
+senha text(50) not null,
+tel varchar(12) not null unique
+);
+
+select * from usuario;
+
+insert into usuario values(null, 'Tiago Fernandes Ribeiro', 'tiagofernandesribeiro@yahoo.com.br', 'senha123', '99999-5979');
