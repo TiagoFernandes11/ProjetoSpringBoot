fb3427b3bf6bf1dab5d48cf29bc3693932f40c42
Tiago Fernandes Ribeiro <tiagofernandesribeiro@yahoo.com.br>
2023-04-05 08:51:44 -0300

    Update UsuarioController.java

--- projeto/src/main/java/br/edu/criandoapi/projeto/controler/UsuarioController.java
+++ projeto/src/main/java/br/edu/criandoapi/projeto/controler/UsuarioController.java
@@ -1,7 +1,6 @@
 package br.edu.criandoapi.projeto.controler;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
